6d2a530b7e20995893110d977dda90f76a18c1d8
Anik Bhattacharya <anikbhattachar27@gmail.com>
2022-12-21 18:20:46 +0530

    first commit

---
+++ Diece game/index.js
@@ -0,0 +1,31 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".jpg"; //dice1.jpg - dice6.jpg
+
+var randomImageSource = "image/" + randomDiceImage; //image/dice1.jpg - images/dice6.jpg
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "image/dice" + randomNumber2 + ".jpg";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+//If player 1 wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "😉 Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "😏 Player 2 Wins!";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw! 😊";
+}
+
+
